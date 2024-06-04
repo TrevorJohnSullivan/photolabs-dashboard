8f7a1dc54062ba5828801aa9f59837e2ca52e1b3
Trevor Sullivan <trevorjohnsullivan@gmail.com>
2024-06-04 11:42:11 -0230

    completed assignment Fetching Appointmnet Data

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,34 +2,42 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
-    focused: null // Add focused key to the state
+    loading: true,
+    focused: null,
+    photos: [],
+    topics: []
   };
 
   componentDidMount() {
@@ -37,6 +45,20 @@ class Dashboard extends Component {
     if (focused) {
       this.setState({ focused });
     }
+
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
+
+    Promise.all(urlsPromise)
+      .then(([photos, topics]) => {
+        this.setState({
+          loading: false,
+          photos,
+          topics
+        });
+      });
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -65,7 +87,7 @@ class Dashboard extends Component {
         <Panel
           key={panel.id}
           label={panel.label}
-          value={panel.value}
+          value={panel.getValue(this.state)}
           onSelect={() => this.selectPanel(panel.id)}
         />
       ));
